ba4b2b93cbd7d5040341964753444130ff439152
liangzhiquan <1298848072@qq.com>
2024-07-16 23:46:19 +0800

    重构后端

---
+++ src/main/java/org/lzq/nyybackend/NyyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.lzq.nyybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lzq/nyybackend/NyyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lzq.nyybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
